0882ed786c57f9e1d2bf583981e3025a9c538c21
EwanMcA <ewan.m@hotmail.com>
2024-06-21 19:00:11 +0100

    Add unit stats, flesh out abilities interaction, and a raft of styling changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,168 @@
-import Unit from './Unit'
+import Unit from "./Unit";
 
 const enum ABILITIES {
-  RAPID_1,
-};
+  ASSAULT = "Assault",
+  DEV_WOUNDS = "Devastating Wounds",
+  HEAVY = "Heavy",
+  ONE_SHOT = "One Shot",
+  PISTOL = "Pistol",
+  RAPID_1 = "Rapid Fire 1",
+  TWIN_LINKED = "Twin-linked",
+}
 
 function App() {
   return (
     <>
       <Unit
         name="Breachers"
-        ranged={[{
-          name: "Pulse Blaster",
-          range: "10\"",
-          attacks: "2",
-          bs: "3+",
-          strength: "6",
-          ap: "-1",
-          damage: "1"
-        }]}
+        stats={{
+          move: '6"',
+          toughness: "3",
+          save: "4+",
+          wounds: "3",
+          LD: "7+",
+          OC: "1",
+        }}
+        ranged={[
+          {
+            name: "Pulse blaster",
+            range: '10"',
+            attacks: "2",
+            bs: "3+",
+            strength: "6",
+            ap: "-1",
+            damage: "1",
+            abilities: [ABILITIES.ASSAULT],
+          },
+          {
+            name: "Pulse pistol",
+            range: '12"',
+            attacks: "1",
+            bs: "4+",
+            strength: "5",
+            ap: "0",
+            damage: "1",
+            abilities: [ABILITIES.PISTOL],
+          },
+        ]}
         abilities={[
-          "Breach and Clear: Each time a model in this unit makes a ranged attack that targets an enemy unit within range of an objective marker, you can re-roll the Wound roll.",
-          "DS8 Support Turret: In your Movement phase, if this unit Remains Stationary, until the start of your next turn, its Shas'ui model is equipped with the support turret weapon.",
+          {
+            name: "Breach and Clear",
+            description:
+              "Each time a model in this unit makes a ranged attack that targets an enemy unit within range of an objective marker, you can re-roll the Wound roll.",
+          },
+          {
+            name: "DS8 Support Turret",
+            description:
+              "In your Movement phase, if this unit Remains Stationary, until the start of your next turn, its Shas'ui model is equipped with the support turret weapon.",
+          },
+          {
+            name: "Guardian Drone",
+            description:
+              "Each time a model makes a ranged attack that targets the bearer's unit, subtract 1 from the Wound roll.",
+          },
+          {
+            name: "Marker Drone",
+            description:
+              "The bearer's unit has the Markerlight keyword and can act as an Observer unit for another unit even if it Advanced this turn.",
+          },
         ]}
       />
       <Unit
         name="Cadre FB"
-        ranged={[{
-          name: "FB Pulse Rifle",
-          range: "30\"",
-          attacks: "1",
-          bs: "3+",
-          strength: "5",
-          ap: "0",
-          damage: "2",
-          abilities: [ABILITIES.RAPID_1],
-        }]}
+        stats={{
+          move: '6"',
+          toughness: "3",
+          save: "4+",
+          wounds: "3",
+          LD: "7+",
+          OC: "1",
+        }}
+        ranged={[
+          {
+            name: "FB Pulse Rifle",
+            range: '30"',
+            attacks: "1",
+            bs: "3+",
+            strength: "5",
+            ap: "0",
+            damage: "2",
+            abilities: [ABILITIES.RAPID_1],
+          },
+        ]}
         abilities={[
-          "Volley Fire: While this model is leading a unit, add 1 to the Attacks characteristic of ranged weapons equipped by models in that unit.",
-          "Crack Shot: Each time this model makes a ranged attack, on a Critical Wound, that attack has an Armour Penetration characteristic of -3."
+          {
+            name: "Volley Fire",
+            description:
+              "While this model is leading a unit, add 1 to the Attacks characteristic of ranged weapons equipped by models in that unit.",
+          },
+          {
+            name: "Crack Shot",
+            description:
+              "Each time this model makes a ranged attack, on a Critical Wound, that attack has an Armour Penetration characteristic of -3.",
+          },
+        ]}
+      />
+      <Unit
+        name="Broadside Battlesuits"
+        stats={{
+          move: '6"',
+          toughness: "3",
+          save: "4+",
+          wounds: "3",
+          LD: "7+",
+          OC: "1",
+        }}
+        ranged={[
+          {
+            name: "Heavy rail rifle",
+            range: '60"',
+            attacks: "2",
+            bs: "4+",
+            strength: "12",
+            ap: "-4",
+            damage: "D6+1",
+            abilities: [ABILITIES.HEAVY, ABILITIES.DEV_WOUNDS],
+          },
+          {
+            name: "Twin plasma rifle",
+            range: '18"',
+            attacks: "1",
+            bs: "4+",
+            strength: "8",
+            ap: "-3",
+            damage: "3",
+            abilities: [ABILITIES.TWIN_LINKED],
+          },
+          {
+            name: "Seeker missile",
+            range: '48"',
+            attacks: "1",
+            bs: "4+",
+            strength: "14",
+            ap: "-3",
+            damage: "D6+1",
+            abilities: [ABILITIES.ONE_SHOT],
+          },
+        ]}
+        abilities={[
+          {
+            name: "Shield Drone",
+            description: "Add 1 to the bearer's Wounds characteristic.",
+          },
+          {
+            name: "Shield Drone",
+            description: "Add 1 to the bearer's Wounds characteristic.",
+          },
+          {
+            name: "Advanced Armour",
+            description:
+              "Models in this unit have the Feel No Pain 4+ ability against mortal wounds.",
+          },
         ]}
       />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Unit.tsx
+++ src/Unit.tsx
@@ -1,50 +1,122 @@
-import styles from './Unit.module.css'
+import { useState } from "react";
+import classNames from "classnames";
+import styles from "./Unit.module.css";
 
-type RangedWeapon = {
-    name: string;
-    range: string;
-    attacks: string;
-    bs: string;
-    strength: string;
-    ap: string;
-    damage: string;
-    abilities: string[];
+type Stats = {
+  move: string;
+  toughness: string;
+  save: string;
+  wounds: string;
+  LD: string;
+  OC: string;
 };
 
-const Unit = ({ name, ranged, abilities = [] }: { name: string, ranged: RangedWeapon[], abilities: string[] }) => {
+type RangedWeapon = {
+  name: string;
+  range: string;
+  attacks: string;
+  bs: string;
+  strength: string;
+  ap: string;
+  damage: string;
+  abilities: string[];
+};
+
+type Ability = {
+  name: string;
+  description: string;
+};
+
+const Unit = ({
+  name,
+  stats,
+  ranged,
+  abilities = [],
+}: {
+  name: string;
+  stats: Stats;
+  ranged: RangedWeapon[];
+  abilities: Ability[];
+}) => {
+  const [open, setOpen] = useState<number>(-1);
+  const toggleOpen = (i: number) => setOpen(open === i ? -1 : i);
+
   return (
-    <div className={styles.card}>
-      <h3 className={styles.name}>{name}</h3>
-      <table>
-        <thead>
-          <tr className={styles.header}>
-            <th scope="col">Weapon</th>
-            <th scope="col">R</th>
-            <th scope="col">A</th>
-            <th scope="col">BS</th>
-            <th scope="col">S</th>
-            <th scope="col">AP</th>
-            <th scope="col">D</th>
-          </tr>
-        </thead>
-        <tbody>
-          { ranged.map((weapon) => (
-            <tr>
-              <th className={styles.weapon} scope="row">{weapon.name}</th>
-              <td>{weapon.range}</td>
-              <td>{weapon.attacks}</td>
-              <td>{weapon.bs}</td>
-              <td>{weapon.strength}</td>
-              <td>{weapon.ap}</td>
-              <td>{weapon.damage}</td>
+    <section className={styles.card}>
+      <div className={styles.card_header}>
+        <h3 className={styles.name}>{name}</h3>
+        <table>
+          <thead>
+            <tr className={styles.header}>
+              <th scope="col">M</th>
+              <th scope="col">T</th>
+              <th scope="col">Sv</th>
+              <th scope="col">W</th>
+              <th scope="col">LD</th>
+              <th scope="col">OC</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-      <ul className={styles.abilities}>
-        {abilities.map((ability) => (<li>{ability}</li>))}
-      </ul>
+            <tr>
+              <td scope="col">{stats.move}</td>
+              <td scope="col">{stats.toughness}</td>
+              <td scope="col">{stats.save}</td>
+              <td scope="col">{stats.wounds}</td>
+              <td scope="col">{stats.LD}</td>
+              <td scope="col">{stats.OC}</td>
+            </tr>
+          </thead>
+        </table>
       </div>
+      <div className={styles.details}>
+        <table>
+          <thead>
+            <tr className={styles.header}>
+              <th scope="col">Weapon</th>
+              <th scope="col">R</th>
+              <th scope="col">A</th>
+              <th scope="col">BS</th>
+              <th scope="col">S</th>
+              <th scope="col">AP</th>
+              <th scope="col">D</th>
+            </tr>
+          </thead>
+          <tbody>
+            {ranged.map((weapon) => (
+              <>
+                <tr>
+                  <th className={styles.weapon} scope="row">
+                    {weapon.name}
+                  </th>
+                  <td>{weapon.range}</td>
+                  <td>{weapon.attacks}</td>
+                  <td>{weapon.bs}</td>
+                  <td>{weapon.strength}</td>
+                  <td>{weapon.ap}</td>
+                  <td>{weapon.damage}</td>
+                </tr>
+                <tr>
+                  <td colspan="7">
+                    {weapon.abilities.map((ability) => (
+                      <span className={styles.weapon_ability}>{ability}</span>
+                    ))}
+                  </td>
+                </tr>
+              </>
+            ))}
+          </tbody>
+        </table>
+        <ul className={styles.abilities}>
+          {abilities.map((ability, i) => (
+            <li
+              onClick={() => toggleOpen(i)}
+              className={classNames({ [styles.closed]: open !== i })}
+            >
+              {open === i ? <>&#x25BF;</> : <>&#x25B5;</>}{" "}
+              <strong>{ability.name}</strong>: {ability.description}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
   );
 };
 
